53c8b5648d69fd33f298c4f4c044e8bd3311a0ff
NomotoYusuke <141814772+NomotoYusuke@users.noreply.github.com>
2023-10-04 19:25:40 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+const correct = `正解です` ;
+const incorrect = `不正解です` ;
+
+function question(){
+    const quiz_1 = document.getElementById(`quiz-1`);
+    //const select = "1問目:" + quiz_1.answer.value + "を選択しました";
+    if (quiz_1.answer.value === 'a') {
+        alert("❌不正解❌ 青虫には葉をいっぱい食べられてツルツルてんになってしまいました");
+      // 正解はB
+    } else if (quiz_1.answer.value === 'b') {
+        alert("💮正解㊗ てんとう虫は何も悪くないです！");
+    } else if (quiz_1.answer.value === 'c') {
+        alert("❌不正解❌ こいつのせいで葉が枝がどんどんダメになりました。気持ち悪い虫です");
+    } else{
+        alert("答えを選択してください");
+    }
+}
+
